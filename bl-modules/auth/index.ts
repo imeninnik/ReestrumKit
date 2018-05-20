859878f9fe87ebafc7a1d106d05379ecd6cf7cb5
Pavel Karlovich <p.karlovich@gmail.com>
2018-05-20 14:43:14 +0300

    [+] add bl core modules

---
+++ bl-modules/auth/index.ts
@@ -0,0 +1,18 @@
+import * as argon2 from "argon2";
+
+
+run();
+
+async function run() {
+
+    try {
+        const hash = await argon2.hash("password");
+        console.log(22, hash);
+
+        const verify = await argon2.verify(hash, "password");
+        console.log(222, verify);
+    } catch (err) {
+        console.log(11, err);
+    }
+
+}
