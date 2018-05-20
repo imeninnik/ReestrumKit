1a5e8d3b7fd88077d5481e9b94c754fc338c47bc
Pavel Karlovich <p.karlovich@gmail.com>
2018-05-20 16:29:27 +0300

    [=] prepare for registering 1

--- bl-modules/register/index.ts
+++ bl-modules/register/index.ts
@@ -2,4 +2,8 @@ export default class Register {
     public static addUser(IBasicUser) {
 
     }
+
+    public static RegisterByEmail(email: string) {
+        console.log('will run Registering by Email');
+    }
 }
